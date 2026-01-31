a6fb0dd1ad15d19aa80007a99bc7941b1dd89d0b
Shreya Sureshbabu Banumathi <shre.jsb@gmail.com>
2026-01-31 17:27:47 -0500

    Add files via upload

---
+++ dashboard.py
@@ -0,0 +1,50 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import os
+from apis import apod_generator
+
+# in the terminal run: streamlit run dashboard.py
+st.title("Water Quality Dashboard")
+st.header("Internship Ready Software Development")
+st.subheader("Shreya Sureshbabu Banumathi")
+st.divider()
+
+df = pd.read_csv("biscayneBay_waterquality.csv")
+
+tab1, tab2, tab3, tab4 = st.tabs(
+    ["Descriptive Statistics", "2D Plots", "3D Plots", "NASA's APOD"]
+)
+
+with tab1:
+    st.write("This tab depicts the unprocessed water quality data gathered from Biscayne Bay.")
+    st.dataframe(df)
+    st.caption("Raw Data")
+    st.divider()
+    st.dataframe(df.describe())
+    st.caption("Descriptive Statistics")
+
+
+with tab2:
+    st.write("This tab shows two-dimensional charts that allow users to anaylze the data visually.")
+    variable = st.selectbox(
+        "Select variable to plot",
+        ["Temperature (c)", "pH", "ODO mg/L"]
+    )
+    fig1 = px.line(df, x = "Time", y = variable)
+    st.plotly_chart(fig1)
+    fig2 = px.scatter(df, x = "ODO mg/L", y = "Temperature (c)", color = "pH")
+    st.plotly_chart(fig2)
+
+with tab3:
+    st.write("This tab allows users to view the data in a three-dimensional plot.")
+    fig3 = px.scatter_3d(df, x = "Longitude", y = "Latitude", z = "Total Water Column (m)", color = "Temperature (c)")
+    fig3.update_scenes(zaxis_autorange="reversed")
+    st.plotly_chart(fig3)
+
+with tab4:
+    st.header("NASA's Astronomy Picture of the Day")
+    url = "https://api.nasa.gov/planetary/apod?api_key="
+    response = apod_generator(url, os.getenv("NASA_API_KEY"))
+    st.image(response["url"], caption="The image above illustrates NASA's Astronomy Picture of the Day.")
+    
